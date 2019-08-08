3d8ba596f7207abdb95494f9da83eccfe92103fe
simrankoul14 <46937000+simrankoul14@users.noreply.github.com>
2019-08-08 23:51:00 +0530

    Add files via upload

---
+++ music/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import Permission, User
+from django.db import models
+
+
+class Album(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    album_title = models.CharField(max_length=500)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.album_title
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
